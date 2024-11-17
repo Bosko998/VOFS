1b58fa5d3be394aeae2056505266df71ca067727
Boško Nikolić <boskonikolic1998@gmail.com>
2024-11-17 21:31:39 +0100

    Dev commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,22 @@
-const AboutPage = () => {
-    return <div>About Us</div>;
+import { StoryblokComponent, getStoryblokApi } from "@storyblok/react/rsc";
+import { SbBlokData } from "@storyblok/react";
+export async function fetchData() {
+  let sbParams: SbBlokData = { version: "draft" };
+
+  const storyblokApi = getStoryblokApi();
+  const response = await storyblokApi.get(`cdn/stories/about`, sbParams);
+
+  return response;
+}
+const AboutPage = async () => {
+  const { data } = await fetchData();
+  return (
+    <div>
+      {data.story.content.blocks.map((blok: SbBlokData) => (
+        <StoryblokComponent blok={blok} key={blok._uid} />
+      ))}
+    </div>
+  );
 };
 
 export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
-import { fetchHomeStaticProps } from "@utils/api";
-import Hero from "@components/Hero";
+import { StoryblokComponent, getStoryblokApi } from "@storyblok/react/rsc";
+import { SbBlokData } from "@storyblok/react";
+export async function fetchData() {
+  let sbParams: SbBlokData = { version: "draft" };
 
-const Home = async () => {
-  const { props } = await fetchHomeStaticProps(); // Call your data fetching function
-  const { story } = props; // Destructure story from props
+  const storyblokApi = getStoryblokApi();
+  const response = await storyblokApi.get(`cdn/stories/home`, sbParams);
+
+  return response;
+}
+export default async function Home() {
+  const { data } = await fetchData();
   return (
-    <>
-      <h1>{story ? story.name : "My Site"}</h1>
-      <Hero />
-    </>
+    <div>
+      {data.story.content.blocks.map((blok: SbBlokData) => (
+        <StoryblokComponent blok={blok} key={blok._uid} />
+      ))}
+    </div>
   );
-};
-export default Home;
+}
