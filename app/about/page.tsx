6222f0e6d82322b13df31dc73e9f655b8d251cf5
Boško Nikolić <boskonikolic1998@gmail.com>
2024-12-12 18:47:02 +0100

    modified:   app/about/page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,22 @@
-import { StoryblokComponent, getStoryblokApi } from "@storyblok/react/rsc";
-import { SbBlokData } from "@storyblok/react";
-export async function fetchData() {
-  const sbParams: SbBlokData = { version: "draft" };
+// import { StoryblokComponent, getStoryblokApi } from "@storyblok/react/rsc";
+// import { SbBlokData } from "@storyblok/react";
+// export async function fetchData() {
+//   const sbParams: SbBlokData = { version: "draft" };
 
-  const storyblokApi = getStoryblokApi();
-  const response = await storyblokApi.get(`cdn/stories/about`, sbParams);
+//   const storyblokApi = getStoryblokApi();
+//   const response = await storyblokApi.get(`cdn/stories/about`, sbParams);
 
-  return response;
-}
-const AboutPage = async () => {
-  const { data } = await fetchData();
-  return (
-    <div>
-      {data.story.content.blocks.map((blok: SbBlokData) => (
-        <StoryblokComponent blok={blok} key={blok._uid} />
-      ))}
-    </div>
-  );
-};
+//   return response;
+// }
+// const AboutPage = async () => {
+//   const { data } = await fetchData();
+//   return (
+//     <div>
+//       {data.story.content.blocks.map((blok: SbBlokData) => (
+//         <StoryblokComponent blok={blok} key={blok._uid} />
+//       ))}
+//     </div>
+//   );
+// };
 
-export default AboutPage;
+// export default AboutPage;
