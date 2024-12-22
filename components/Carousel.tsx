d92712734e2bcdd1c70f754d516c71d90572f5a9
Bosko998 <boskonikolic1998@gmail.com>
2024-12-22 21:42:58 +0100

    Dev commit

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,30 +1,32 @@
-"use client"
-import {useState} from 'react'
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBeforeOutlined";
-
-function Carousel() {
-    const [image,setImage] = useState("/front_image.webp")
-    const changeImage = (dir:string)=>{
-        if(dir =='left'){
-            setImage("/hero-img.png");
-        }else{
-            setImage("/front_image.webp")
-        }
+type CarouselProps = {
+  image: {
+    filename: string;
+    id: number;
+  };
+};
+function Carousel({ image }: CarouselProps) {
+  const { id, filename } = image;
+  // 19406101 First pic
+  // 19406173 Second
+  // 19406605 Third
+  // Dynamic height calculation based on `id`
+  function positionElements(id: number) {
+    let css = "";
+    if (id == 19406173) {
+      css = "h-[350px] z-10";
+    } else {
+      css = `h-[300px] z-0 relative opacity-60`;
+      css += id == 19406101 ? " left-16" : " right-16";
     }
+    return css;
+  }
+
   return (
-    <div className="relative flex items-center justify-center">
-      <img src={image} alt="pic1" className="w-[750px]" />
-      <NavigateBeforeIcon
-        sx={{ fontSize: 50 }}
-        className='absolute left-1 cursor-pointer animate-breathing'
-        onClick={()=>changeImage('left')}
-      />
-      
-      <NavigateNextIcon
-        sx={{ fontSize: 50 }}
-        className="absolute right-1 cursor-pointer animate-breathing"
-        onClick={()=>changeImage('right')}
+    <div className={`block w-max lef ${positionElements(id)}`}>
+      <img
+        className="w-full h-full rounded-2xl object-cover"
+        src={filename}
+        alt="ProjectImg"
       />
     </div>
   );
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,25 @@
 import Carousel from './Carousel'
-
-function Projects() {
+import {storyblokEditable} from '@storyblok/react/rsc'
+type Image = {
+  id: number;
+  filename: string;
+};
+type CarouselProps = {
+  blok: {
+    images: Array<Image>; 
+    _uid: string;          
+  };
+}
+function Projects({blok}:CarouselProps) {
   return(
-    <section>
-      <div className="flex justify-center items-center mb-4 mt-4">
-        <Carousel/>
-      </div>
+    <section className='mt-16 mb-16'>
+      <div {...storyblokEditable(blok)} key={blok._uid} data-test="carousel" className='relative flex justify-center items-center'>
+        
+      {blok.images.map((image)=>( 
+        <Carousel key={image.id} image={image}/>
+      ))}
+    </div>
+    
     </section>
   );
 }
