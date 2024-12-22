7f21f7c60179eb27eb5f64f3d8f8fc117fd48546
Bosko998 <boskonikolic1998@gmail.com>
2024-12-22 21:45:41 +0100

    Dev commit

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 export { default as NavBar } from "./NavBar";
 export { default as Footer } from "./Footer";
-export { default as CustomButton } from "./CustomButton";
 export { default as Page } from "./Page";
 export { default as Grid } from "./Grid";
 export { default as Teaser } from "./Teaser";
